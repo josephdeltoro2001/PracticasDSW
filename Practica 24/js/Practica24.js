f5164594259241114f814121b8acf1e11f6ebde1
josephdeltoro2001 <158236768+josephdeltoro2001@users.noreply.github.com>
2024-04-01 19:31:01 -0700

    Se agrega la practica 24

---
+++ Practica 24/js/Practica24.js
@@ -0,0 +1,14 @@
+//Del Toro Cruz Joseph Vladimir
+function AvisodeHuracán(hayUnHuracan) {
+    if (hayUnHuracan) {
+        alert("Hay un Huracán");
+    } else {
+        alert("Está Soleado");
+    }
+}
+
+//Se manda a llamar con el parámetro hayUnHuracan = true
+AvisodeHuracán(true);
+
+//Se manda a llamar con el parámetro hayUnHuracan = false 
+AvisodeHuracán(false);
